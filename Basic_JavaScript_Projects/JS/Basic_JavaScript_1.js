25b3bb02cb0be36b7a7a4c58fabc9310340da315
salvak17 <67027287+salvak17@users.noreply.github.com>
2020-07-13 19:32:40 -0700

    Add Basic_JavaScript_Projects folder
    Added my Basic_JavaScript_Projects folder to my JavaScript Projects repository

---
+++ Basic_JavaScript_Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,33 @@
+document.write("Hello, World! "); //writes "Hello, World! " in my HTML doc
+
+var A = "This is a string. ";
+document.write(A); //writes variable A, "which is This is a string. "
+
+/* Assignment 1 */
+var a = "Boo!";
+window.alert(a); //alerts in the window variable a, which is "Boo!"
+
+/* Assignment 2 (disabled for code used for assignment 3) */
+/* document.write("\"Zoinks!\", yells Shaggy. "); */ //writes ""Zoinks!", yells Shaggy. "
+
+/* Assignment 3 */
+document.write("\"Zoinks!\"" + ", yells Shaggy. "); //writes ""Zoinks!", yells Shaggy. "
+
+/* Challenge 1 */
+var B = "Mitochondria is" + " the powerhouse of the cell. ";
+document.write(B); //writes variable B, which is "Mitochondria is the powerhouse of the cell. "
+
+/* Assignment 4 */
+var TeamRWBY = "Ruby R., Weiss S., Blake B., Yang L. ", R = "Ruby Rose ", W = "Weiss Schnee ", B = "Blake Belladonna ", Y = "Yang Xiao Long ";
+document.write(TeamRWBY); //writes variable TeamRWBY, which is "Ruby R., Weiss S., Blake B., Yang L. "
+
+/* Assignment 5 */
+var TeamRWBY = TeamRWBY.fontcolor("grey");
+var R = R.fontcolor("red");
+var W = W.fontcolor("white");
+var B = B.fontcolor("black");
+var Y = Y.fontcolor("yellow");
+document.write(R); //writes variable R in red font color, which is "Ruby Rose "
+
+/* Assignment 6 (wrote expression in statement) */
+document.write(3 + 3); //writes the expression 3 + 3, which is 6
